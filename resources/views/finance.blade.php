6ca032ef6482ee99a48faa18a0da5d5b46262afa
carlosramonDev <carlosramonDev@gmail.com>
2021-10-13 16:47:11 -0300

    first commit

---
+++ resources/views/finance.blade.php
@@ -0,0 +1,1 @@
+<h1>Página de finanças<h1>
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Controle Financeiro</title>
+
+        <!-- css -->
+        <link rel="stylesheet" href="/css/style.css">
+
+    </head>
+    <body class="antialiased">
+        <h1>Home</h1>
+        
+    </body>
+</html>
