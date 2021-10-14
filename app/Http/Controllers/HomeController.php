ca54c1d4dff79d13ddb9ecd9d667020c91c54802
carlosramonDev <carlosramonDev@gmail.com>
2021-10-14 20:21:53 -0300

    Home controller

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    
+      /*HOME*/
+
+    public function index() {
+        
+        
+        return view('welcome');
+    }
+
+    
+}
