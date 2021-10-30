d2d332870b87c9fdf548e62d44ab58577a8640f3
Jhonatanmizu <natanjesuss20@gmail.com>
2021-10-29 23:22:01 -0300

    Added widget

---
+++ src/components/Widget.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="widget">
+      <h3>{{title}}</h3>
+      <p>{{number}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Widget',
+  props: {
+    title: String,
+    number: Number
+  }
+}
+</script>
+
+<style>
+.widget{
+    text-align: center;
+    width: 27.9rem;
+    height:12.2rem;
+    background-color:#282A36;
+    border-radius:1rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    color: #fff;
+    overflow: hidden;
+}
+.widget h3{
+    border-bottom: 1px solid var(--primary-color);
+    width:100%;
+    margin-bottom: 1rem;
+}
+.widget p{
+    font-weight: bold;
+}
+</style>
