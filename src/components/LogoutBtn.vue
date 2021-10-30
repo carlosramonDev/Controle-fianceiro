f59bf7976d4a1beda952583047057715224cd88b
Jhonatanmizu <natanjesuss20@gmail.com>
2021-10-29 23:21:05 -0300

    Added Logout btn

---
+++ src/components/LogoutBtn.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+      <button class="btn" type="submit" @click="logout">Logout <UilSignout/></button>
+  </div>
+</template>
+
+<script>
+import { UilSignout } from '@iconscout/vue-unicons'
+export default {
+  name: 'Logout',
+  components: {
+    UilSignout
+  },
+  methods: {
+    logout: function (e) {
+      e.preventDefault()
+    }
+  }
+
+}
+</script>
+
+<style>
+.btn {
+  padding:.8rem;
+  font-family: 'Oswald', sans-serif;
+  font-weight: 700;
+  background-color:var(--primary-color);
+  color: #f8f8f8;
+  border:none;
+  border-radius: .5rem;
+
+}
+</style>
